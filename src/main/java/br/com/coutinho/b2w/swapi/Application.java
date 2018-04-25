e0756ae267cba772c68484345a442fc58ade390b
Rafael Coutinho <rafael.coutinho@ebix.com>
2018-04-25 18:19:50 -0300

    Project and packages rename. Eclipse configuration.

---
+++ src/main/java/br/com/coutinho/b2w/swapi/Application.java
@@ -0,0 +1,21 @@
+package br.com.coutinho.b2w.swapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/** Simple class to start up the application.
+ *
+ * @SpringBootApplication adds:
+ *  @Configuration
+ *  @EnableAutoConfiguration
+ *  @ComponentScan
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
